d91851bc8f5c1978a992053c99c4757fb00b769e
CarlosJ09 <carlos.ml.jimenezz@gmail.com>
2022-07-06 12:47:34 -0400

    Añadiendo Ecommerce

---
+++ ECommerce/ECommerce/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using ECommerce;
+using System.Net;
+
+namespace ECommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        private superCarrosEntities db = new superCarrosEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Category).Include(p => p.Mark).Include(p => p.Supplier);
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+    }
+}
